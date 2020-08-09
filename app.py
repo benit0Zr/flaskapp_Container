0b7fb751dd4fb4ee07a33897b17fac8f35f08574
Benito sanchez <sanchezrbenito@gmail.com>
2020-08-09 13:44:13 -0500

    app Web

--- app.py
+++ app.py
@@ -161,30 +161,32 @@ def update_Alumno(matricula):
     #return render_template("alumnos_update.html", form=form, user=user)
 
 
-@app.route("/update", methods=["GET", "POST"])
+@app.route('/update ', methods=["GET", "POST"])
 def updateAlumnos():    
+    user = session.get('profile')
+    if not user:
+        return redirect(url_for('login'))
 
-    form = upForm()
-    
-    if form.validate_on_submit():
-        alumnos = {
-
-            'matricula': form.matricula.data,
-            'nombre': form.nombre.data,
-            'apellidos': form.apellidos.data,
-            'genero': form.genero.data,
-            'carrera': form.carrera.data,
-            'generacion': form.generacion.data,
-            'status': form.estatus.data,
-            'domicilio': form.domicilio.data,
-            'telefono': form.tel.data,
-            'correo': form.correo.data,
-        }
-        
-        mongodb.alumnos.insert(alumnos)        
-        return redirect(url_for("updateAlumnos", form=form))
+    if request.method == 'POST':
+        mongodb.alumnos.update_one(
+            {'matricula' : request.form['matricula'] } , {
+                "$set":{
+                    'matricula': request.form['matricula'],
+                    'nombre': request.form['nombre'],
+                    'apellidos': request.form['apellidos'],
+                    'genero': request.form['genero'],
+                    'carrera': request.form['carrera'],
+                    'generacion': request.form['generacion'],
+                    'status': request.form['estatus'],
+                    'domicilio': request.form['domicilio'],
+                    'telefono': request.form['telefono'],
+                    'correo': request.form['correo'],
+                }
+            }
+        )  
+        return redirect(url_for("consulta_Alum"))
            
-    return render_template("alumnos_update.html")
+    #return render_template("consulta_Alumnos.html", user=user)
 
 
 @app.route('/admin')
@@ -203,7 +205,56 @@ def admin():
     return redirect(url_for("admin"))
 
 
-   
+# ruta para eliminar admin
+@app.route('/admin/delete')
+@app.route('/admin/delete/<username>/', methods=['GET'])
+def delete_Admin(username):
+    delete = mongodb.users
+    user = delete.find_one({'username':username})
+    delete.remove(user)
+    return redirect(url_for('admin'))
+
+
+#ruta para actualizar admin
+@app.route('/admin/actualizar')
+@app.route('/admin/actualizar/<username>/', methods=["GET"])
+def update_Admin(username):
+
+    user = session.get('profile')
+    if not user:
+        return redirect(url_for('login'))
+
+    form = SignupForm()
+    
+    admin = mongodb.users.find_one({'username' : username})
+    
+    #return str(alumno)
+    return render_template('admin_update.html', admin=admin, user=user, form=form)
+    #return render_template("alumnos_update.html", form=form, user=user)
+
+@app.route('/up ', methods=["GET", "POST"])
+def up_admin():    
+    user = session.get('profile')
+    if not user:
+        return redirect(url_for('login'))
+
+    if request.method == 'POST':
+        mongodb.users.update_one(
+            {'username' : request.form['username'] } , {
+                "$set":{
+                    
+                    'nombre': request.form['nombre'],
+                    'apellidos': request.form['apellidos'],
+                    'sexo': request.form['sexo'],
+                    'email': request.form['email']
+                }
+            }
+        )  
+        return redirect(url_for("admin"))
+
+
+
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5090, debug=True)
